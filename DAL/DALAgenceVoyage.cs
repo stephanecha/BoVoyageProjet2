f0c563daa54acbc21271ac54d16970a59b31e409
YoosZ <so_yoos@msn.com>
2018-08-30 15:44:03 +0200

    Creation des requetes de AgenceVoyage et Destination avec Entity

---
+++ DAL/DALAgenceVoyage.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Class;
+
+namespace DAL
+{
+    public class DALAgenceVoyage : IDALAgenceVoyage
+    {
+        public void AjouterAgenceVoyage(AgenceVoyage agenceVoyage)
+        {
+            using (Context context = new Context())
+            {
+                context.AgencesVoyages.Add(agenceVoyage);
+                context.SaveChanges();
+            }
+        }
+
+        public AgenceVoyage ChoisirAgenceVoyage(int id)
+        {
+            using (Context context = new Context())
+            {
+                return context.AgencesVoyages.Single(x => x.Id == id);
+            }
+        }
+
+        public IEnumerable<AgenceVoyage> FiltrerAgenceVoyage(string filtre)
+        {
+            using (Context context = new Context())
+            {
+                return context.AgencesVoyages
+                                    .Where(x => x.Nom.Contains(filtre))
+                                    .ToList();
+            }
+        }
+
+        public IEnumerable<AgenceVoyage> ListerAgenceVoyage()
+        {
+            using (Context context = new Context())
+            {
+                return context.AgencesVoyages.ToList();
+            }
+        }
+
+        public void ModifierAgenceVoyage(AgenceVoyage agenceVoyage)
+        {
+            using (Context context = new Context())
+            {
+                context.AgencesVoyages.Attach(agenceVoyage);
+                context.Entry(agenceVoyage).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void SupprimerAgenceVoyage(AgenceVoyage agenceVoyage)
+        {
+            using (Context context = new Context())
+            {
+                context.AgencesVoyages.Attach(agenceVoyage);
+                context.AgencesVoyages.Remove(agenceVoyage);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DAL/DALDestination.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Class;
+
+namespace DAL
+{
+    public class DALDestination : IDALDestination
+    {
+        public void AjouterDestination(Destination destination)
+        {
+            using (Context context = new Context())
+            {
+                context.Destinations.Add(destination);
+                context.SaveChanges();
+            }
+        }
+
+        public Destination ChoisirDestination(int id)
+        {
+            using (Context context = new Context())
+            {
+                return context.Destinations.Single(x => x.Id == id);
+            }
+        }
+
+        public IEnumerable<Destination> FiltrerDestination(string filtre)
+        {
+            using (Context context = new Context())
+            {
+                return context.Destinations
+                                    .Where(x => x.Continent.Contains(filtre)
+                                     || x.Pays.Contains(filtre)
+                                     || x.Region.Contains(filtre)
+                                     )
+                                    .ToList();
+            }
+        }
+
+        public IEnumerable<Destination> ListerDestination()
+        {
+            using (Context context = new Context())
+            {
+                return context.Destinations.ToList();
+            }
+        }
+
+        public void ModifierDestination(Destination destination)
+        {
+            using (Context context = new Context())
+            {
+                context.Destinations.Attach(destination);
+                context.Entry(destination).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void SupprimerDestination(Destination destination)
+        {
+            using (Context context = new Context())
+            {
+                context.Destinations.Attach(destination);
+                context.Destinations.Remove(destination);
+                context.SaveChanges();
+            }
+        }
+    }
+}
