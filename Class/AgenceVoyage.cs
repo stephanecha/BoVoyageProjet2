f7a44391f34e196973523cf73ae682e1461c1bfb
YoosZ <so_yoos@msn.com>
2018-08-30 15:43:29 +0200

    Modification des classes AgenceVoyage et Destination pour les rendre compatible avec Entity

--- Class/AgenceVoyage.cs
+++ Class/AgenceVoyage.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Class
 {
+    [Table("AgencesVoyages")]
     public class AgenceVoyage
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nom { get; set; }
+
+        public virtual ICollection<Voyage> Voyages { get; set; } 
 
         public AgenceVoyage (string nom)
         {
--- Class/Destination.cs
+++ Class/Destination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Class
@@ -7,18 +8,21 @@ namespace Class
     public class Destination
     {
         public int Id { get; set; }
+
+        [Required]
         public string Continent { get; set; }
+
+        [Required]
         public string Pays { get; set; }
+
+        [Required]
         public string Region { get; set; }
+
         public string Description { get; set; }
 
-        public Destination(string continent ,string pays, string region)
-        {
-            Continent = continent;
-            Pays = pays;
-            Region = region;
-        }
-        public Destination(string continent ,string pays, string region, string description)
+        public virtual ICollection<Voyage> Voyages { get; set; }
+
+        public Destination(string continent, string pays, string region, string description = null)
         {
             Continent = continent;
             Pays = pays;
