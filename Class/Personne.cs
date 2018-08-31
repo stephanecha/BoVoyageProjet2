f16558f527a472a53e9aed0d11c114dd194485fe
YoosZ <so_yoos@msn.com>
2018-08-31 13:44:03 +0200

    Implémentation du Framework Entity avec les [required] La propriété Age n'est plus dans la base de données mais est calculée à la volée

--- Class/Personne.cs
+++ Class/Personne.cs
@@ -1,18 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Class
 {
     public abstract class Personne
     {
         public int Id { get; set; }
+
+        [Required]
         public string Civilite { get; set; }
+
+        [Required]
         public string Nom { get; set; }
+
+        [Required]
         public string Prenom { get; set; }
+
+        [Required]
         public string Adresse { get; set; }
+
+        [Required]
         public string Telephone { get; set; }
+
+        [Required]
         public DateTime DateNaissance { get; set; }
-        public int Age { get; }
+
+        [NotMapped]
+        public int Age
+        {
+            get
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - DateNaissance.Year;
+                return age;
+            }
+        }
     }
 }
