c780dcd7adb96e99a5df9faff05893f230f82e6b
YoosZ <so_yoos@msn.com>
2018-08-31 13:38:16 +0200

    Modification de la méthode Afficher en AfficherListe Et implémantation du service client

--- BoVoyageProjet2/ModuleClient.cs
+++ BoVoyageProjet2/ModuleClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using BoVoyage.Framework.UI;
 using Class;
+using Service;
 
 namespace BoVoyageProjet2APP
 {
@@ -13,14 +14,14 @@ namespace BoVoyageProjet2APP
             new List<InformationAffichage>
             {
                 InformationAffichage.Creer<Client>(x=>x.Id, "Id", 3),
-                InformationAffichage.Creer<Client>(x=>x.Civilite, "Civilite", 3),
+                InformationAffichage.Creer<Client>(x=>x.Civilite, "Civilite", 14),
                 InformationAffichage.Creer<Client>(x=>x.Nom, "Nom", 10),
-                InformationAffichage.Creer<Client>(x=>x.Prenom, "Prenom", 10),
-                InformationAffichage.Creer<Client>(x=>x.Adresse, "Adresse", 10),
-                InformationAffichage.Creer<Client>(x=>x.Telephone, "Telephone", 10),
+                InformationAffichage.Creer<Client>(x=>x.Prenom, "Prenom", 15),
+                InformationAffichage.Creer<Client>(x=>x.Adresse, "Adresse", 20),
+                InformationAffichage.Creer<Client>(x=>x.Telephone, "Tel", 15),
                 InformationAffichage.Creer<Client>(x=>x.Email, "Email", 15),
-                InformationAffichage.Creer<Client>(x=>x.DateNaissance, "Date de Naissance", 10),
-                InformationAffichage.Creer<Client>(x=>x.Age, "Age", 10),
+                InformationAffichage.Creer<Client>(x=>x.DateNaissance, "Date Nais.", 10),
+                InformationAffichage.Creer<Client>(x=>x.Age, "Age", 2),
             };
 
         private IEnumerable<Client> liste = new List<Client>();
@@ -35,7 +36,7 @@ namespace BoVoyageProjet2APP
         {
             menu.AjouterElement(new ElementMenu("1", "Afficher")
             {
-                FonctionAExecuter = this.Afficher
+                FonctionAExecuter = this.AfficherListe
             });
             menu.AjouterElement(new ElementMenu("2", "Nouveau")
             {
@@ -62,9 +63,13 @@ namespace BoVoyageProjet2APP
             throw new NotImplementedException();
         }
 
-        private void Afficher()
+        private void AfficherListe()
         {
             ConsoleHelper.AfficherEntete("Afficher");
+
+            ServiceClient service = new ServiceClient();
+
+            this.liste = service.ListerClient();
 
             ConsoleHelper.AfficherListe(this.liste, strategieAffichageClients);
         }
@@ -79,10 +84,13 @@ namespace BoVoyageProjet2APP
                 nom : ConsoleSaisie.SaisirChaineObligatoire("Nom ?"),
                 prenom : ConsoleSaisie.SaisirChaineObligatoire("Prenom ?"),
                 email : ConsoleSaisie.SaisirChaineObligatoire("Email ?"),
-                dateNaissance : ConsoleSaisie.SaisirDateObligatoire("Date de Naissance ?")
+                dateNaissance : ConsoleSaisie.SaisirDateObligatoire("Date de Naissance ?"),
+                adresse : ConsoleSaisie.SaisirChaineObligatoire("Adresse ?"),
+                telephone : ConsoleSaisie.SaisirChaineObligatoire("Téléphone ?")
             );
 
-            // this.liste.Add(client);
+            ServiceClient service = new ServiceClient();
+            service.AjouterClient(client);
         }
     }
 }
